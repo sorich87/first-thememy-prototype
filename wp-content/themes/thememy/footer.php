db34c652b45ab1af7f12879bc002937a2972bec4
Ulrich Sossou <sorich87@gmail.com>
2012-04-12 22:53:25 +0100

    Removed powered by WordPress text and bootstrap styles

--- wp-content/themes/thememy/footer.php
+++ wp-content/themes/thememy/footer.php
@@ -13,31 +13,13 @@
 
       <footer>
         <?php do_action( 'thememy_credits' ); ?>
-				<p>
-					<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'thememy' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'thememy' ), 'WordPress' ); ?></a>
-					<span class="sep"> | </span>
-					&copy; ThemeMY! 2012
-				</p>
+        <p>
+          Copyright &copy; 2012
+          <a href="<?php esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( 'The easiest way to sell WordPress themes' ); ?>">ThemeMY!</a>
+        </p>
       </footer>
 
     </div> <!-- /container -->	
-
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap-transition.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap-alert.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap-modal.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap-dropdown.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap-scrollspy.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap-tab.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap-tooltip.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap-popover.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap-button.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap-collapse.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap-carousel.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap-typeahead.js"></script>
 
 	<?php wp_footer(); ?>
 

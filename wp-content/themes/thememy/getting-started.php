5f4a3ea0736771223c83ed3aface39ad0afd1184
Ulrich Sossou <sorich87@gmail.com>
2012-04-25 08:36:48 +0100

    Moved getting started code to template file

---
+++ wp-content/themes/thememy/getting-started.php
@@ -0,0 +1,55 @@
+<div class="modal hide" id="getting-started">
+  <div class="modal-header">
+    <a class="close" data-dismiss="modal" href="#getting-started">×</a>
+		<h3><?php _e( 'Getting Started' ); ?></h3>
+  </div>
+  <div class="modal-body">
+		<p><?php _e( 'Start selling your themes in three (3) easy steps.' ); ?></p>
+
+		<h4>
+			<?php _e( 'Step 1' ); ?>
+			<?php if ( thememy_get_settings() ) : ?>
+				<span class="label label-success"><?php _e( 'done' ); ?></span>
+			<?php endif; ?>
+		</h4>
+		<p><?php printf(
+			__( 'Go to <a href="%s">the settings page</a> and fill in your business, pricing and payment details.' ),
+			add_query_arg( 'getting_started', 'true', site_url( 'settings/' ) )
+		); ?></p>
+
+		<h4>
+			<?php _e( 'Step 2' ); ?>
+			<?php if ( get_posts( array( 'post_type' => 'td_theme', 'author' => get_current_user_id(), 'fields' => 'ids' ) ) ) : ?>
+				<span class="label label-success"><?php _e( 'done' ); ?></span>
+			<?php endif; ?>
+		</h4>
+		<p><?php printf(
+			__( 'Go to <a href="%s">the themes page</a> and upload the themes you want to sell.' ),
+			add_query_arg( 'getting_started', 'true', site_url( 'themes/' ) )
+		); ?></p>
+
+		<h4><?php _e( 'Step 3' ); ?></h4>
+		<p><?php _e( 'Copy the themes purchase links and paste them in your website.' ); ?></p>
+  </div>
+  <div class="modal-footer">
+		<a href="#" class="btn" data-dismiss="modal"><?php _e( 'Close' ); ?></a>
+		<a href="#" class="btn btn-warning" id="no-getting-started"><?php _e( "Don't show this again" ); ?></a>
+  </div>
+</div>
+<script type="text/javascript">
+jQuery(function ($) {
+	$("#getting-started").modal();
+
+	$("#no-getting-started").click(function (e) {
+		e.preventDefault();
+
+		var data = {
+			_ajax_nonce: "<?php echo wp_create_nonce( 'thememy-no-getting-started' ); ?>",
+			action: "thememy-no-getting-started"
+		};
+		$.post( "<?php echo admin_url( 'admin-ajax.php' ); ?>", data );
+
+		$("#getting-started").modal( "hide" );
+	});
+});
+</script>
